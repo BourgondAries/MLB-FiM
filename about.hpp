4fca0fa9f915351b4657e9acfa0fb7be3f26c9cc
Bourgond Aries <macocio@gmail.com>
2016-03-12 01:57:37 +0100

    Modernize about

---
+++ about.hpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+
+class About {
+	sf::Text noter, thanks, progry;
+	sf::Texture logo1[6];
+	sf::Sprite logo2[6];
+	sf::RectangleShape fader, lineh, linev;
+	sf::RenderWindow &window;
+
+public:
+
+	About(sf::RenderWindow &window, const sf::Font &font);
+	enter(sf::Sprite &screenS);
+
+};
